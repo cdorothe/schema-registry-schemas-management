d17ffaa1d553775fe7c9c282ebff4ec0fd0607bd
Christophe DOROTHE <kristophe.dorothe@gmail.com>
2021-05-20 20:54:18 +0200

    Add detailed comments and code cleaning

--- src/main/java/com/chdor/schema_registry/schemas/management/avro/GenerateAvroSchema.java
+++ src/main/java/com/chdor/schema_registry/schemas/management/avro/GenerateAvroSchema.java
@@ -26,7 +26,7 @@ public class GenerateAvroSchema {
 	 * Generates AVRO Schemas using Confluent Schema Registry API
 	 */
 	public static void generateAvroSchemaFromPojoWithSchemaRegistryAPI() {
-		// Generate an AVRO Schema from our class: TVShowAvroPojo()
+		// Generate an AVRO Schema from our class: TVShowAvroPojo
 		// The last parameter is to choose to remove Java Properties or not.		
 		// By default Avro uses CharSequence for the String representation, the following syntax allows you to overwrite the default behavior
 		// and use java.lang.String as the String type for the instances of the fields declared like this:
@@ -34,20 +34,23 @@ public class GenerateAvroSchema {
         //   "type": "string",
         //   "avro.java.string": "String"
         // }
+
+		// Using AvroSchemaUtils to infer the AVRO Schema from our class
 		Schema schema = AvroSchemaUtils.getSchema(new TVShowAvroPojo(), true, true);
 		System.out.println("AVRO Schema generated from POJO with Confluent Schema Registry API (AvroSchemaUtils):\n"
 				+ schema.toString(true));
 		
+		// Using AvroSchemaUtils to infer the AVRO Schema from our class
 		AvroSchema avroSchema = new AvroSchema(AvroSchemaUtils.getSchema(new TVShowAvroPojo(), true, true));
 		System.out.println("AVRO Schema generated from from POJO with Confluent AvroSchema API:\n"
 				+ avroSchema.rawSchema().toString(true));
 	}
 
 	/**
-	 * Generates AVRO Schemas using Apcahe Avro API
+	 * Generates AVRO Schemas using Apache Avro API
 	 */
 	public static void generateAvroSchemaFromPojoWithAvroAPI() {
-		// Generates AVRO Schema from POJO TVSeriesActor AVro annoted class
+		// Generates AVRO Schema from TVShowAvroPojo Avro Pojo annoted class
 		Schema schema = ReflectData.get().getSchema(TVShowAvroPojo.class);
 		System.out.println("AVRO Schema generated from POJO with AVRO API:\n" + schema.toString(true));
 	}
