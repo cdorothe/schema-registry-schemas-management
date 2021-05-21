70047d69c0154e8ebd824834d49e3ea68ec12edf
Christophe DOROTHE <kristophe.dorothe@gmail.com>
2021-05-21 15:44:10 +0200

    Rebuild to be compliant with TVShow.avsc

--- src/main/java/com/chdor/schema_registry/schemas/management/avro/model/TVShowAvroPojo.java
+++ src/main/java/com/chdor/schema_registry/schemas/management/avro/model/TVShowAvroPojo.java
@@ -2,35 +2,21 @@ package com.chdor.schema_registry.schemas.management.avro.model;
 
 import java.util.List;
 
-import org.apache.avro.reflect.AvroAlias;
 import org.apache.avro.reflect.AvroDoc;
-import org.apache.avro.reflect.AvroEncode;
-import org.apache.avro.reflect.AvroIgnore;
-import org.apache.avro.reflect.AvroMeta;
-import org.apache.avro.reflect.AvroSchema;
-import org.apache.avro.reflect.DateAsLongEncoding;
 
-@AvroAlias(alias = "TVShow",space = "com.chdor.schema_registry.schemas.management.avro.model")
 @AvroDoc(value = "Descriptive card of series of the year 1970- 1980")
 public class TVShowAvroPojo {
 		
-		@AvroSchema(value = "\"string\"")
 		@AvroDoc(value = "Name of the Serie")
 		private String name = null;
 		
-		@AvroDoc(value = "Serie release date")
-		private String releaseDate = null;
+		@AvroDoc(value = "Release Date of the Serie")
+		private String date = null;
 
-		@AvroDoc(value = "A tiny Series character identity card")
+		@AvroDoc(value = "A tiny Serie character identity card")
 		private List<CharacterAvroPojo> characters = null;
 
-		@AvroIgnore()
-		private String actor = null;
-
-		@AvroEncode(using = DateAsLongEncoding.class)
-		@AvroDoc(value = "number of milliseconds since January 1, 1970, 00:00:00 GMT")
-		@AvroMeta(key = "note", value = "A simple note")
-		private Long date = null;
+		// Getters & *Setters
 		
 		public String getName() {
 			return name;
@@ -39,21 +25,13 @@ public class TVShowAvroPojo {
 		public void setName(String name) {
 			this.name = name;
 		}
-		public TVShowAvroPojo withName(String name) {
-			this.name = name;
-			return this;
+
+		public String getDate() {
+			return date;
 		}
 
-		public String getReleaseDate() {
-			return releaseDate;
-		}
-
-		public void setReleaseDate(String releaseDate) {
-			this.releaseDate = releaseDate;
-		}
-		public TVShowAvroPojo withReleaseDate(String releaseDate) {
-			this.releaseDate = releaseDate;
-			return this;
+		public void setDate(String date) {
+			this.date = date;
 		}
 
 		public List<CharacterAvroPojo> getCharacters() {
@@ -64,5 +42,6 @@ public class TVShowAvroPojo {
 			this.characters = characters;
 		}
 
+		
 		
 }
