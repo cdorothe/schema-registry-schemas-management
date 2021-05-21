6bf3e227fb524f6e12d2ecf948a16cef8c5c8a05
Christophe DOROTHE <kristophe.dorothe@gmail.com>
2021-05-21 20:27:59 +0200

    Make the method splitCamelCase(String s) static

--- src/main/java/com/chdor/schema_registry/schemas/management/json/PrintJsonSchema.java
+++ src/main/java/com/chdor/schema_registry/schemas/management/json/PrintJsonSchema.java
@@ -85,7 +85,7 @@ public class PrintJsonSchema {
 	}
 	
 
-	public String splitCamelCase(String s) {
+	public static String splitCamelCase(String s) {
 		return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])",
 				"(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
 	}
