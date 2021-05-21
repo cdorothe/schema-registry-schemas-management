623cf508751b3c75b037b3aebc0e83ea7947d244
Christophe DOROTHE <kristophe.dorothe@gmail.com>
2021-05-21 17:19:02 +0200

    Clean code

--- src/main/java/com/chdor/schema_registry/schemas/management/avro/CreateAvroRecord.java
+++ src/main/java/com/chdor/schema_registry/schemas/management/avro/CreateAvroRecord.java
@@ -7,9 +7,8 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 
-import com.chdor.schema_registry.schemas.management.avro.model.Character;
 import com.chdor.schema_registry.schemas.management.avro.model.TVShow;
-
+import com.chdor.schema_registry.schemas.management.avro.model.Character;
 
 /**
  * A simple class used to create Avro records based on Avro Schemas
@@ -41,7 +40,7 @@ public class CreateAvroRecord {
 		System.out.println("Create an AVRO record using the generated AVRO Java classes:\n" + tvShow.toString());
 		System.out.println("- AVRO Schema used:\n" + tvShow.getSchema().toString(true)+"\n");
 		
-		// Create Record using a generic instance of a record schema
+		// Create Record using a "dynamic" schema
 		String mySchema = "{\"type\":\"record\",\"name\":\"TVSeriesActor\",\"namespace\":\"com.chdor.schema_registry.example.avro.model\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"}]}";
 		Schema.Parser parser = new Schema.Parser();
 		Schema schema = parser.parse(mySchema);
