1d914b5fd1c990c36f0b56544fc5edc23ac57a49
Christophe DOROTHE <kristophe.dorothe@gmail.com>
2021-05-27 00:26:32 +0200

    Rebuild comments and add JsonSchema pretty print

--- src/main/java/com/chdor/schema_registry/schemas/management/json/GenerateJsonSchema.java
+++ src/main/java/com/chdor/schema_registry/schemas/management/json/GenerateJsonSchema.java
@@ -2,7 +2,9 @@ package com.chdor.schema_registry.schemas.management.json;
 
 import java.util.function.Supplier;
 
-import com.chdor.schema_registry.schemas.management.json.model.TVShowPojoAnnoted;
+import org.json.JSONObject;
+
+import com.chdor.schema_registry.schemas.management.json.model.TVShowPojo;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kjetland.jackson.jsonSchema.JsonSchemaConfig;
@@ -16,10 +18,12 @@ import io.confluent.kafka.schemaregistry.json.JsonSchema;
 import io.confluent.kafka.schemaregistry.json.JsonSchemaUtils;
 import io.confluent.kafka.schemaregistry.json.SpecificationVersion;
 import scala.Option;
+
 /**
  * A simple class used to generate JSON Schema from POJO
+ * 
  * @author Christophe Dorothé</br>
- * Contact: kristophe.dorothe@gmail.com
+ *         Contact: kristophe.dorothe@gmail.com
  *
  */
 public class GenerateJsonSchema {
@@ -31,26 +35,27 @@ public class GenerateJsonSchema {
 
 	/**
 	 * Generate JsonSchema with Confluent SchemaRegistry API
+	 * 
 	 * @throws Exception
 	 */
 	public static void generateJsonSchemaWithConfluentSchemaRegistry() throws Exception {
 		// Default basic JSON Schema Generation from our TVShowPojo class
-		JsonSchema jsonSchema = JsonSchemaUtils.getSchema(new TVShowPojoAnnoted());
+		JsonSchema jsonSchema = JsonSchemaUtils.getSchema(new TVShowPojo());
 		System.out.println("JSON Schema generated from POJO with Confluent Schema Registry:\n"
 				+ jsonSchema.toJsonNode().toPrettyString());
 
-		// Disable the oneOf declaration 
+		// Disable the oneOf declaration
 		CachedSchemaRegistryClient cachedSchemaRegistryClient = new CachedSchemaRegistryClient("http://weatherwax:8081",
 				5);
 
 		// Generate our JSON Schema
-		jsonSchema = JsonSchemaUtils.getSchema(new TVShowPojoAnnoted(), SpecificationVersion.DRAFT_7, false,
+		jsonSchema = JsonSchemaUtils.getSchema(new TVShowPojo(), SpecificationVersion.DRAFT_7, false,
 				cachedSchemaRegistryClient);
 
 		System.out.println("JSON Schema generated from POJO with Confluent Schema Registry (oneOf is disabled):\n"
 				+ jsonSchema.toJsonNode().toPrettyString());
 	}
-	
+
 	/**
 	 * Generate JsonSchema with Jackson Json SchemaGenerator API
 	 */
@@ -61,9 +66,10 @@ public class GenerateJsonSchema {
 		// By default, without control, the JSON Schema Title name is build using
 		// camelcase
 		Boolean autoGenerateTitleForProperties = false;
-		// By this way we can specify the array dfault format
+		
+		// Defines the array default format
 		Option<String> defaultArrayFormat = new Option<String>() {
-			
+
 			/**
 			 * 
 			 */
@@ -74,29 +80,29 @@ public class GenerateJsonSchema {
 				// TODO Auto-generated method stub
 				return false;
 			}
-			
+
 			@Override
 			public Object productElement(int n) {
 				// TODO Auto-generated method stub
 				return null;
 			}
-			
+
 			@Override
 			public int productArity() {
 				// TODO Auto-generated method stub
 				return 0;
 			}
-			
+
 			@Override
 			public String get() {
 				// TODO Auto-generated method stub
-				return "CharacterPojo Schema";
+				return "CharacterPojo Record";
 			}
 		};
-		
-		// Enable or disable 'oneOf' for options declaration 
+
+		// Enable or disable 'oneOf' for options declaration
 		Boolean useOneOfForOption = false;
-		// Enable or disable 'oneOf' declaration 
+		// Enable or disable 'oneOf' declaration
 		Boolean useOneOfForNullables = false;
 		// Enable or disable the 'propertyOrder' declaration
 		Boolean usePropertyOrdering = true;
@@ -108,20 +114,24 @@ public class GenerateJsonSchema {
 		scala.collection.immutable.Map<String, String> customType2FormatMapping = new scala.collection.immutable.HashMap<String, String>();
 		// https://github.com/jdorn/json-editor/issues/709
 		Boolean useMultipleEditorSelectViaProperty = false;
-		// If rendering array and type is instanceOf class in this set, then we add 'uniqueItems": true' to schema - See // https://github.com/jdorn/json-editor for more info
+		// If rendering array and type is instanceOf class in this set, then we add
+		// 'uniqueItems": true' to schema - See // https://github.com/jdorn/json-editor
+		// for more info
 		scala.collection.immutable.Set<Class<?>> uniqueItemClasses = new scala.collection.immutable.HashSet<>();
-		// Can be used to prevent rendering using polymorphism for specific classes. 
+		// Can be used to prevent rendering using polymorphism for specific classes.
 		// Set to empty map to prevent exception
 		scala.collection.immutable.Map<Class<?>, Class<?>> classTypeReMapping = new scala.collection.immutable.HashMap<Class<?>, Class<?>>();
-		// Suppliers in this map can be accessed using @JsonSchemaInject(jsonSupplierViaLookup = "lookupKey")
+		// Suppliers in this map can be accessed using
+		// @JsonSchemaInject(jsonSupplierViaLookup = "lookupKey")
 		scala.collection.immutable.Map<String, Supplier<JsonNode>> jsonSuppliers = null;
 		// Using default impl that scans entire classpath
 		SubclassesResolver subclassesResolver = new SubclassesResolverImpl();
 		// Set "additionalProperties" to false
 		Boolean failOnUnknownProperties = true;
-		// Used to match against different validation-groups (javax.validation.constraints)
+		// Used to match against different validation-groups
+		// (javax.validation.constraints)
 		Class<?>[] javaxValidationGroups = new Class<?>[0];
-		// I select the $schema draft version
+		// Select the $schema draft version
 		JsonSchemaDraft jsonSchemaDraft = JsonSchemaDraft.DRAFT_07;
 
 		// Build Json Schema Config
@@ -132,23 +142,24 @@ public class GenerateJsonSchema {
 				subclassesResolver, failOnUnknownProperties, javaxValidationGroups, jsonSchemaDraft);
 		jsonSchemaConfig.withJsonSchemaDraft(jsonSchemaDraft);
 
-		// Build our JsonSchemaGenerator with our configuration 
+		// Build our JsonSchemaGenerator with our configuration
 		JsonSchemaGenerator jsonSchemaGenerator = new JsonSchemaGenerator(mapper, jsonSchemaConfig);
-		// Defines the Title Schema. Prevents the camelCase transformation
+		// Defines the Title Schema. Disable the camelCase transformation
 		String title = "TVShow";
 		// Defines a Schema Description
 		String description = "Descriptive card of series of the year 1970- 1980";
 		// Generate our JSON Schema from TVShowPojo.class using our JsonSchemaGenerator
-		JsonNode schemaJsonNode = jsonSchemaGenerator.generateJsonSchema(TVShowPojoAnnoted.class, title, description);
-		System.out.println(
-				"JSON Schema generated from POJO with Jackson jsonSchema Generator:\n" + schemaJsonNode.toPrettyString());
-		
-		// Convert the  generated Schema into a JsonSchema
+		JsonNode schemaJsonNode = jsonSchemaGenerator.generateJsonSchema(TVShowPojo.class, title, description);
+		//System.out.println("JSON Schema generated from POJO with Jackson jsonSchema Generator:\n"
+		//		+ schemaJsonNode.toPrettyString());
+
+		// Convert the generated Schema into a JsonSchema
 		JsonSchema jsonSchema = new JsonSchema(schemaJsonNode);
 		// Fails if schema is invalid
 		jsonSchema.validate();
 		
-		System.out.println(
-				"JSON Schema generated converts into JsonSchema:\n" + jsonSchema.rawSchema().toString());
+		// Display the JsonSchema inferred from our TVShowPojo class
+		System.out.println("JSON Schema generated converts into JsonSchema:\n" + new JSONObject(jsonSchema.toString()).toString(2)); 
+		
 	}
 }
